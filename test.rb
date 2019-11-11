a87600cfd027815add1b20e60f56594f3553fd5b
TheRealSavi <JohnGibbons167@gmail.com>
2019-11-11 02:29:29 -0500

    added an example for an hsv color picker

--- test.rb
+++ test.rb
@@ -1,45 +1,64 @@
-require 'ruby2d'
-require_relative 'Element/hsv.rb'
+require_relative 'savio.rb'
 
-view = Square.new(
+$view = Square.new(
   x:10,
   y:10,
   size: 400
 )
-label = Text.new(
+$label = Text.new(
   'value',
   x:430, y:10
 )
 
-mySlider = Slider.new(
+$myAngle = Slider.new(
   x:10, y:430,
   min:0,
   max:360,
-  length:500
+  length:500,
+  value: rand(0..360)
 )
-mySat = Slider.new(
+$mySaturation = Slider.new(
   x:450, y:80,
   min:0.0,
   max:1.0,
-  length:100
+  length:100,
+  value: rand(0.0..1.0)
 )
-myVal = Slider.new(
+$myValue = Slider.new(
   x:450, y:120,
   min:0.0,
   max:1.0,
-  length:100
+  length:100,
+  value: rand(0.0..1.0)
 )
 
-temp = 0
-update do
-  temp += 1
-  temp = temp % 360
-  mySlider.setValue(temp)
-  hue = hsv2rgb(mySlider.value,mySat.value,myVal.value)
-  view.color.r = hue[0]
-  view.color.g = hue[1]
-  view.color.b = hue[2]
+$mySettings = ButtonList.new(type: 'checkbox')
+
+$mySettings.addOption(displayName: 'Loop Angle?', x: 450, y: 240, id: 'doLoop')
+$mySettings.addOption(displayName: 'Randomize?', x: 450, y: 280, id: 'doRandom')
+
+def updateView()
+  hue = hsv2rgb($myAngle.value,$mySaturation.value,$myValue.value)
+  $view.color.r = hue[0]
+  $view.color.g = hue[1]
+  $view.color.b = hue[2]
+
   text = ('%0.2f' % hue[0].to_s) + "," + ('%0.2f' % hue[1].to_s) + "," + ('%0.2f' % hue[2].to_s)
-  label.text = text
+  $label.text = text
 end
+
+update do
+  if $mySettings.selected.include?('doLoop')
+    $myAngle.setValue(($myAngle.value + 1 ) % 360)
+  end
+
+  if $mySettings.selected.include?('doRandom')
+    $myAngle.setValue(rand(0..360))
+    $mySaturation.setValue(rand(0.0..1.0))
+    $myValue.setValue(rand(0.0..1.0))
+  end
+
+  updateView()
+end
+
 show
