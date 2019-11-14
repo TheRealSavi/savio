2a83691e0e8b1183d9a5747e370c169c99683c83
TheRealSavi <JohnGibbons167@gmail.com>
2019-11-14 02:38:24 -0500

    added handler for ColorSlider

--- Element/io.rb
+++ Element/io.rb
@@ -1,3 +1,4 @@
+#ButtonList Handler
 on :mouse_down do |event|
   if event.button == :left
     ButtonList.buttonLists.each do |buttonList|
@@ -13,6 +14,7 @@ on :mouse_down do |event|
   end
 end
 
+#Slider Handler
 on :mouse do |event|
   if event.button == :left && event.type == :down
     @dragging = true
@@ -30,3 +32,48 @@ on :mouse do |event|
     end
   end
 end
+
+#ColorPicker Handler
+on :mouse do |event|
+  if event.button == :left && event.type == :down
+    @dragging = true
+  end
+  if event.button == :left && event.type == :up
+    @dragging = false
+  end
+
+  if @dragging == true
+    ColorSlider.sliders.each do |slider|
+      if slider.shown && slider.enabled
+
+        distance = (event.x-slider.x.to_f) ** 2 + (event.y-slider.y.to_f) ** 2
+        distance = Math.sqrt(distance)
+
+        if distance <= slider.size.to_f * 0.7
+          slider.showOptions
+        else
+          slider.hideOptions
+        end
+
+        if distance >= slider.size * 0.90 && distance <= slider.size * 1.15
+          if event.x - slider.x == 0
+            event.x += 0.01
+          end
+          angle = Math.atan((event.y.to_f - slider.y.to_f) / (event.x.to_f - slider.x.to_f)).to_f
+          angle = angle.to_f * 180.0/Math::PI
+
+          if (event.x.to_f - slider.x.to_f) < 0
+            angle += 180.0
+          end
+          if event.y.to_f - slider.y.to_f < 0 && (event.x.to_f - slider.x.to_f) > 0
+            angle += 360.0
+          end
+
+          slider.setValue(angle.to_f)
+        end
+      end
+    end
+  end
+end
+
+#Animation Handler
