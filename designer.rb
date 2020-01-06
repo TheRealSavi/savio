11596443fa9650f1c2c66dc080470107ee8ba130
TheRealSavi <JohnGibbons167@gmail.com>
2020-01-06 05:28:02 -0500

    added a color slider

--- designer.rb
+++ designer.rb
@@ -7,14 +7,28 @@ $sidebarButtonManager = ButtonList.new()
 
 def makeSidebar()
   seperatorLine = Line.new(x1: 800, y1: 0, x2: 800, y2: 800)
+
   templateSlider = Slider.new(x: 830, y: 40, length: 220, draggingEnabled: true, dragType: "duplicate", knobColor: "blue")
   templateButton = $sidebarButtonManager.addOption(x: 830, y: 90, displayName: "Radio ButtonList", id: "j")
   templateButton = $sidebarButtonManager.addOption(x: 830, y: 150, displayName: "Checkbox ButtonList", id: "o")
-  listLine = Line.new(x1: 800, y1: 180, x2: 1100, y2: 180)
-  listText = Text.new("ButtonList's : ", x: 810, y: 190)
-  inputBox = InputBox.new(x: 810, y: 220, size: 20, draggingEnabled: true, dragType: "duplicate")
+  inputBox = InputBox.new(x: 830, y: 180, size: 20, draggingEnabled: true, dragType: "duplicate")
+  colorPicker = ColorSlider.new(x: 940, y: 300)
+
+  listLine = Line.new(x1: 800, y1: 420, x2: 1100, y2: 420)
+  listText = Text.new("ButtonList's : ", x: 810, y: 430)
+
 end
 
 makeSidebar()
+
+update do
+  ColorSlider.sliders.each do |slider|
+    if slider.animating == true
+      slider.animate
+    end
+  end
+
+
+end
 
 show()
