0e0d7efd60f8cce887640ec62a081bd11fddde00
TheRealSavi <JohnGibbons167@gmail.com>
2020-01-09 01:04:52 -0500

    added support for new buttons

--- Element/io.rb
+++ Element/io.rb
@@ -1,64 +1,103 @@
+#Adding ability to sense dragging
+on :mouse do |event|
+  if @dragging == nil
+    @dragging = {}
+  end
+  if event.type == :down
+    @dragging[event.button] = true
+  end
+  if event.type == :up
+    @dragging[event.button] = false
+  end
+end
+
 #ButtonList Handler
-on :mouse_down do |event|
-  if event.button == :left
-    ButtonList.buttonLists.each do |buttonList|
-      buttonList.optionID.each do |id|
-        button = buttonList.touch(id)
-        if button[:shown] && button[:enabled]
-          if event.x.between?(button[:x]-button[:size],button[:x]+button[:size]) && event.y.between?(button[:y]-button[:size],button[:y]+button[:size])
-            buttonList.toggle(id)
-          end
+on :mouse do |event|
+
+  if @dragging[:right] == false
+    Button.buttons.each do |button|
+      if button.draggingEnabled
+        button.endDrag()
+      end
+    end
+  end
+
+  if @dragging[:right] == true
+    Button.buttons.each do |button|
+
+      if button.draggingEnabled
+        if event.x.between?(button.x-button.size,button.x+button.size) && event.y.between?(button.y-button.size,button.y+button.size)
+          button.allowDrag = true
+        end
+        if button.allowDrag
+          button.drag(event.x, event.y)
         end
       end
     end
   end
+
+  if @dragging[:left] == true
+    Button.buttons.each do |button|
+
+      if button.shown && button.enabled
+        if event.x.between?(button.x-button.size,button.x+button.size) && event.y.between?(button.y-button.size,button.y+button.size)
+          if button.buttonManager == nil
+            button.toggle
+          else
+            button.buttonManager.toggle(button)
+          end
+        end
+      end
+
+    end
+  end
+
 end
 
 #Slider Handler
 on :mouse do |event|
-  if event.button == :left && event.type == :down
-    @dragging = true
-  end
 
-  if event.button == :left && event.type == :up
-    @dragging = false
+  if @dragging[:right] == false
     Slider.sliders.each do |slider|
       if slider.draggingEnabled
         slider.endDrag()
       end
     end
   end
-  
-  if @dragging == true
+
+  if @dragging[:right] == true
     Slider.sliders.each do |slider|
+
       if slider.draggingEnabled
-        if event.y.between?(slider.y-slider.size,slider.y+slider.size)
-          if event.x.between?(slider.x,slider.x+slider.length)
-            slider.allowDrag = true
-          end
+        if event.y.between?(slider.y-slider.size,slider.y+slider.size) && event.x.between?(slider.x,slider.x+slider.length)
+          slider.allowDrag = true
         end
         if slider.allowDrag
           slider.drag(event.x, event.y)
         end
-      else
-        if slider.shown && slider.enabled
-          if event.y.between?(slider.y-slider.size,slider.y+slider.size)
-            slider.moveKnob(event.x)
-          end
-        end
       end
+
     end
   end
+
+  if @dragging[:left] == true
+    Slider.sliders.each do |slider|
+
+      if slider.shown && slider.enabled
+        if event.y.between?(slider.y-slider.size,slider.y+slider.size)
+          slider.moveKnob(event.x)
+        end
+      end
+
+    end
+  end
+
 end
 
 #Inputbox Handler
 on :mouse do |event|
-  if event.button == :left && event.type == :down
-    @dragging = true
-  end
 
-  if event.button == :left && event.type == :up
-    @dragging = false
+  if @dragging[:right] == false
     InputBox.inputBoxs.each do |inputBox|
       if inputBox.draggingEnabled
         inputBox.endDrag()
@@ -66,30 +105,35 @@ on :mouse do |event|
     end
   end
 
-  if @dragging == true
+  if @dragging[:right] == true
     InputBox.inputBoxs.each do |inputBox|
+
       if inputBox.draggingEnabled
-        if event.y.between?(inputBox.y,inputBox.y+inputBox.height)
-          if event.x.between?(inputBox.x,inputBox.x+inputBox.length)
-            inputBox.allowDrag = true
-          end
+        if event.y.between?(inputBox.y,inputBox.y+inputBox.height) && event.x.between?(inputBox.x,inputBox.x+inputBox.length)
+          inputBox.allowDrag = true
         end
         if inputBox.allowDrag
           inputBox.drag(event.x, event.y)
         end
-      else
-        if inputBox.shown && inputBox.enabled
-          if event.y.between?(inputBox.y,inputBox.y+inputBox.height)
-            if event.x.between?(inputBox.x,inputBox.x+inputBox.length)
-              inputBox.select
-            end
-          else
-            inputBox.deselect
-          end
-        end
       end
+
     end
   end
+
+  if @dragging[:left] == true
+    InputBox.inputBoxs.each do |inputBox|
+
+      if inputBox.shown && inputBox.enabled
+        if event.y.between?(inputBox.y,inputBox.y+inputBox.height) && event.x.between?(inputBox.x,inputBox.x+inputBox.length)
+          inputBox.select
+        else
+          inputBox.deselect
+        end
+      end
+
+    end
+  end
+
 end
 
 on :key_down do |event|
@@ -102,14 +146,8 @@ end
 
 #ColorPicker Handler
 on :mouse do |event|
-  if event.button == :left && event.type == :down
-    @dragging = true
-  end
-  if event.button == :left && event.type == :up
-    @dragging = false
-  end
 
-  if @dragging == true
+  if @dragging[:left] == true
     ColorSlider.sliders.each do |slider|
       if slider.shown && slider.enabled
 
--- designer.rb
+++ designer.rb
@@ -1,34 +1,70 @@
 require_relative 'savio'
 set width: 1100, height: 800
-$buttonManager = ButtonList.new()
-$sliderManager = {}
 
-$sidebarButtonManager = ButtonList.new()
+class Sidebar
+  attr_accessor :elements, :sidebarButtonManager
 
-def makeSidebar()
-  seperatorLine = Line.new(x1: 800, y1: 0, x2: 800, y2: 800)
+  def initialize()
+    @elements = {}
+    @buttonManager = ButtonManager.new(type: "radio")
+    @mode = true
 
-  templateSlider = Slider.new(x: 830, y: 40, length: 220, draggingEnabled: true, dragType: "duplicate", knobColor: "blue")
-  templateButton = $sidebarButtonManager.addOption(x: 830, y: 90, displayName: "Radio ButtonList", id: "j")
-  templateButton = $sidebarButtonManager.addOption(x: 830, y: 150, displayName: "Checkbox ButtonList", id: "o")
-  inputBox = InputBox.new(x: 830, y: 180, size: 20, draggingEnabled: true, dragType: "duplicate")
-  colorPicker = ColorSlider.new(x: 940, y: 300)
+    @elements[:lineEdge] = Line.new(
+      x1: 800, y1: 0,
+      x2: 800, y2: 800
+    )
 
-  listLine = Line.new(x1: 800, y1: 420, x2: 1100, y2: 420)
-  listText = Text.new("ButtonList's : ", x: 810, y: 430)
+    @elements[:templateSlide] = Slider.new(
+      x: 830, y: 40, length: 220,
+      draggingEnabled: true, dragType: "duplicate"
+    )
+
+    @elements[:templateRadio] = Button.new(
+      x: 830, y: 90,
+      displayName: "Radio ButtonManager",
+      buttonManager: @buttonManager,
+      draggingEnabled: true, dragType: "duplicate"
+    )
+
+    @elements[:templateCheck] = Button.new(
+      x: 830, y: 150,
+      displayName: "Checkbox ButtonManager",
+      buttonManager: @buttonManager,
+      draggingEnabled: true, dragType: "duplicate"
+    )
+
+    @elements[:templateInput] = InputBox.new(
+      x: 830, y: 180, size: 20,
+      draggingEnabled: true, dragType: "duplicate"
+    )
+
+    @elements[:templateColor] = ColorSlider.new(
+      x: 940, y: 300
+    )
+
+    @elements[:lineList] = Line.new(
+      x1: 800, y1: 420,
+      x2: 1100, y2: 420
+    )
+
+    @elements[:lineText] = Text.new("ButtonList's : ",
+      x: 810, y: 430
+    )
+  end
 
 end
 
-makeSidebar()
+$sidebar = Sidebar.new()
 
 update do
+
+
+
   ColorSlider.sliders.each do |slider|
     if slider.animating == true
       slider.animate
     end
   end
-
-
 end
 
 show()
