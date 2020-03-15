d2a87d15c6d84bc1b77341291822bd3333989c86
TheRealSavi <JohnGibbons167@gmail.com>
2020-03-15 01:46:14 -0400

    Added Colors

---
+++ lib/savio/Colors.rb
@@ -0,0 +1,13 @@
+module Savio
+  module Colors
+    White = '#FDFDFD'
+    Gray  = '#858585'
+    Black = '#1A1A1A'
+
+    Red    = '#FF728B'
+    Green  = '#28E457'
+    Blue   = '#28B2E4'
+    Yellow = '#FFED61'
+    Purple = '#FF90FF'
+  end
+end
